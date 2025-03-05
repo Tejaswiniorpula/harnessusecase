5873e681b42896a28090118ad152d7207a1c1e1d
Tejaswiniorpula <142099450+Tejaswiniorpula@users.noreply.github.com>
2025-03-05 17:37:01 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  project = "lyrical-bus-452711-c5"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "centos_vm" {
+  name         = "my-vm2"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-stream-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
